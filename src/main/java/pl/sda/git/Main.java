4f78cddcb7ec61bec17ff948f7ca56894a19ec64
Andrzej  Niczyporuk <admin@vraz.pl>
2019-02-07 14:23:10 +0100

    5 add Java

--- src/main/java/pl/sda/git/Main.java
+++ src/main/java/pl/sda/git/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	
 	static {
 		possibleActions.put("concat", new ConcatAction());
+		possibleActions.put("5-AddJava", new AddJavaAction());
 	}
 	
 	public static void main(String[] args) {
