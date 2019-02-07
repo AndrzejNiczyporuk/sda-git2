4237c2b3b362c3d4a045e0073dac8aa15de8ab17
Andrzej  Niczyporuk <admin@vraz.pl>
2019-02-07 13:57:23 +0100

    Add 1-sort

---
+++ src/main/java/pl/sda/git/impl/SortAlphaAction.java
@@ -0,0 +1,14 @@
+package pl.sda.git.impl;
+
+import pl.sda.git.Action;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortAlphaAction implements Action {
+    public String doIt(List<String> actionInput) {
+        return actionInput.stream()
+                .sorted()
+                .collect(Collectors.joining(" "));
+    }
+}
