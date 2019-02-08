afeeebe1204971816bc4e8e14267081eb5db0c1c
Andrzej  Niczyporuk <admin@vraz.pl>
2019-02-08 12:49:40 +0100

    13 dodaj dlugosci wszystkich elementow

--- src/main/java/pl/sda/git/Main.java
+++ src/main/java/pl/sda/git/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	
 	static {
 		possibleActions.put("concat", new ConcatAction());
+		possibleActions.put("13-AddAllLengths", new AddAllLengthsAction());
 	}
 	
 	public static void main(String[] args) {
